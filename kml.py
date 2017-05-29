a2ebd386ef28d79b9c7382a97a580f39f99e711b
Paul Ellis <golgothen@gmail.com>
2017-05-29 16:39:56 +1000

    Added LatLonBox object

--- kml.py
+++ kml.py
@@ -14,15 +14,27 @@ class Point(object):
         self.lat = lat
         self.lon = lon
         self.alt = alt
-        self.parent = parent
-        if self.parent is not None:
-            self.depth = self.parent.depth + 1
+        self.__parent = parent
+        if self.__parent is not None:
+            self.depth = self.__parent.depth + 1
+        else:
+            self.depth = 0
+
+    @property
+    def parent(self):
+        return self.__parent
+
+    @parent.setter
+    def parent(self, parent):
+        self.__parent = parent
+        if self.__parent is not None:
+            self.depth = self.__parent.depth + 1
         else:
             self.depth = 0
 
     def __str__(self):
         tmp = (' ' * self.depth) + '<Point>\n'
-        tmp += (' ' * self.depth) + ' <coordinates>{},{},{}</coordinates>\n'.format(self.lat, self.lon, 0 if self.alt is None else self.alt)
+        tmp += (' ' * self.depth) + '  <coordinates>{},{},{}</coordinates>\n'.format(self.lat, self.lon, 0 if self.alt is None else self.alt)
         tmp += (' ' * self.depth) + '</Point>\n'
         return tmp
 
@@ -48,42 +60,119 @@ class Placemark(object):
             raise ValueError('Name must contain a value')
         self.name = name
         self.desc = '' if desc is None else desc
-        self.parent = parent
-        if self.parent is None:
-            self.depth = 0
+        self.__parent = parent
+        if self.__parent is None:
+            self.__depth = 0
         else:
-            self.depth = self.parent.depth + 1
-        self.point = point
-        if self.point is not None:
-            self.point.depth = self.depth + 1
+            self.__depth = self.__parent.depth + 1
+        self.__point = point
 
     def __str__(self):
         if self.point is None:
-            raise ValueError('Point value has not been set')
-        tmp = (' ' * self.depth) + '<Placemark>\n'
-        tmp += (' ' * self.depth) + ' <name>{}</name>\n'.format(self.name)
-        tmp += (' ' * self.depth) + ' <description>{}</description>\n'.format(self.desc)
-        tmp += (' ' * self.depth) + str(self.point)
-        tmp += (' ' * self.depth) + '</Placemark>\n'
+            raise ValueError('Point value has not been set for Placemarker {}'.format(self.name))
+        tmp = (' ' * self.__depth) + '<Placemark>\n'
+        tmp += (' ' * self.__depth) + ' <name>{}</name>\n'.format(self.name)
+        tmp += (' ' * self.__depth) + ' <description>{}</description>\n'.format(self.desc)
+        tmp += (' ' * self.__depth) + str(self.__point)
+        tmp += (' ' * self.__depth) + '</Placemark>\n'
         return tmp
 
     def __eq__(self, p):
-        return self.point == p.point
+        return self.__point == p.point
 
     def __ne__(self, p):
-        return self.point != p.point
+        return self.__point != p.point
+
+    @property
+    def depth(self):
+        return self.__depth
+
+    @depth.setter
+    def depth(self, depth):
+        self.__depth = depth
+        if self.__point is not None:
+            self.__point.depth = self.__depth + 1
+
+    @property
+    def parent(self):
+        return self.__parent
+
+    @parent.setter
+    def parent(self, parent):
+        self.__parent = parent
+        if self.__parent is not None:
+            self.depth = self.__parent.depth + 1
+        else:
+            self.depth = 0
+
+    @property
+    def point(self):
+        return self.__point
+
+    @point.setter
+    def point(self, point):
+        self.__point = point
+        if self.__point is not None:
+            self.__point.depth = self.__depth + 1
+
+class LatLonBox(object):
+    def __init__(self, north, south, east, west, rotation = None, parent = None):
+        if north > 90.0 or north < -90.0:
+            raise ValueError('North out of bounds')
+        if south > 90.0 or south < -90.0:
+            raise ValueError('South out of bounds')
+        if east > 180.0 or east < -180.0:
+            raise ValueError('East out of bounds')
+        if west > 180.0 or west < -180.0:
+            raise ValueError('West out of bounds')
+        if rotation > 0:                          # Counterclockwise rotation
+            rotation = rotation % 360.0           # Remove full rotations (if any)
+            if rotation > 180.0:                  # Check for greater than a semi-rotation
+                rotation = rotation % -180.0      # reverse direction o rotation
+        else:                                     # Clockwise rotation
+            rotation = rotation % -360.0          # Remove full rotations (if any)
+            if rotation < 180.0:                  # Check for greater than a semi-rotation
+                rotation = rotation % 180.0       # Reverse direction of rotation
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+        self.rotation = rotation
+        if parent is not None:
+            self.__parent = parent
+            self.depth = self.__parent.depth + 1
+
+    def __contains__(self, p):
+        inside_lon = False
+        if self.north > self.south:
+            if p.lon < self.north and p.lon > self.south:
+                inside_lon = True
+        else:
+            if p.lon > self.north and p.lon < self.south:
+                inside_lon = True
+        inside_lat = False
+        if self.east > self.west:
+            if p.lat < self.east and p.lat > self.west:
+                inside_lat = True
+        else:
+            if p.lat > self.east and p.lat < self.west:
+                inside_lat = True
+        return (inside_lon and inside_lat)
 
 class Document(object):
     def __init__(self):
         self.__list = []
+        self.depth = 0
 
     def append(self, item):
+        item.parent = self
         self.__list.append(item)
 
     def remove(self, item):
         self.__list.remove(item)
 
     def insert(self, position, item):
+        item.parent = self
         self.__list.insert(position, item)
 
     def __len__(self):
@@ -94,10 +183,22 @@ class Document(object):
 
     def __iter__(self):
         self.__iter_counter = 0
-        return self.__list[self.__iter_counter]
+        for x in self.__list:
+            yield x #self.__list[self.__iter_counter]
 
-    def __next__(self):
-        self.__iter_counter +=1
-        if self.__iter_counter > len(self.__list):
-            raise StopIteration
-        return self.__list[self.__iter_counter]
+    def __getitem__(self, index):
+        return self.__list[index]
+
+#    def __next__(self):
+#        self.__iter_counter +=1
+#        if self.__iter_counter > len(self.__list):
+#            raise StopIteration
+#        yield self.__list[self.__iter_counter]
+
+    def __str__(self):
+        tmp = '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2">\n'
+        for l in self.__list:
+            tmp += str(l)
+        tmp+='</kml>\n'
+        return tmp
+
